2117fefe625e61229c3fba6e4527d92985a43d31
Sören Weber <mail@soeren-weber.de>
2021-10-08 19:30:27 +0200

    security: fix XSS for malicioius URLs #114

--- static/js/relearn.js
+++ static/js/relearn.js
@@ -510,10 +510,8 @@ jQuery(function() {
         var element = $(this);
         var url = encodeURI(document.location.origin + document.location.pathname);
         var link = url + "#"+element[0].id;
-        return " <span class='anchor' data-clipboard-text='"+link+"'>" +
-            "<i class='fas fa-link fa-lg'></i>" +
-            "</span>"
-        ;
+        var html = " " + $( "<span>" ).addClass("anchor").attr("data-clipboard-text", link).append("<i class='fas fa-link fa-lg'></i>").get(0).outerHTML;
+        return html;
     });
 
     $(".anchor").on('mouseleave', function(e) {
