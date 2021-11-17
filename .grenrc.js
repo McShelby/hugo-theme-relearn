b84b4157bcfe33c11fc43a720b78802ebfc3acb8
Sören Weber <mail@soeren-weber.de>
2021-11-17 16:51:15 +0100

    changelog: fix handling of issues containing multiple labels

--- .grenrc.js
+++ .grenrc.js
@@ -16,10 +16,12 @@ module.exports = {
     ],
   },
   ignoreLabels: [
+    "hugo",
+  ],
+  ignoreIssuesWith: [
     "discussion",
     "documentation",
     "duplicate",
-    "hugo",
     "invalid",
     "support",
     "wontfix",
