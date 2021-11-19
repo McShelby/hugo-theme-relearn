8bcdb5d9b9fc0f3b3e4e532036ea14ea08ee5216
McShelby <McShelby@users.noreply.github.com>
2021-11-19 22:41:49 +0000

    deploy: b91b555aa02dea98bba31bc383d007d99e337b97

--- js/auto-complete.js
+++ js/auto-complete.js
@@ -2,6 +2,7 @@
     JavaScript autoComplete v1.0.4
 		#46 - positioning
 		#75 - complete
+        McShelby/hugo-theme-relearn#155
     Copyright (c) 2014 Simon Steinberger / Pixabay
     GitHub: https://github.com/Pixabay/JavaScript-autoComplete
     License: http://www.opensource.org/licenses/mit-license.php
