98cc7904abebb975332de6ecd8df1bef3a51d8b9
Sören Weber <mail@soeren-weber.de>
2023-05-19 11:09:05 +0200

    tags: improve search index for tags #531

--- layouts/_default/index.search.js
+++ layouts/_default/index.search.js
@@ -1,8 +1,21 @@
 {{- partialCached "page-meta.hugo" . .RelPermalink }}
 {{- $pages := slice }}
 {{- range .Site.Pages }}
-{{- if and .Title (or (ne (.Scratch.Get "relearnIsHiddenStem") true) (ne .Site.Params.disableSearchHiddenPages true) ) }}
-{{- $pages = $pages | append (dict "uri" (partial "relLangPrettyUglyURL.hugo" (dict "to" .)) "title" .Title "tags" .Params.tags "description" .Description "content" (.Plain | htmlUnescape)) }}
-{{- end }}
+  {{- if and .Title (or (ne (.Scratch.Get "relearnIsHiddenStem") true) (ne .Site.Params.disableSearchHiddenPages true) ) }}
+    {{- $title := .Title }}
+    {{- if eq .Kind "taxonomy" }}
+      {{- $title = i18n .Data.Plural }}
+      {{- if not $title }}
+        {{- $title = .Data.Plural | humanize }}
+      {{- end }}
+    {{- else if eq .Kind "term" }}
+      {{- $title = i18n .Data.Singular }}
+      {{- if not $title }}
+        {{- $title = .Data.Singular | humanize }}
+      {{- end }}
+      {{- $title = printf "%s %s %s" $title (default "::" .Site.Params.titleSeparator) (.Title | humanize) }}
+    {{- end }}
+    {{- $pages = $pages | append (dict "uri" (partial "relLangPrettyUglyURL.hugo" (dict "to" .)) "title" $title "tags" .Params.tags "description" .Description "content" (.Plain | htmlUnescape)) }}
+  {{- end }}
 {{- end -}}
 var relearn_search_index = {{ $pages | jsonify (dict "indent" "  ") }}
