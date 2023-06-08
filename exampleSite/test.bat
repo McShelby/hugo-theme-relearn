2f010f394487515821b46319dde3efbfc243dd81
Sören Weber <mail@soeren-weber.de>
2023-06-08 17:59:03 +0200

    test: add file list

--- exampleSite/test.bat
+++ exampleSite/test.bat
@@ -1,11 +1,22 @@
 @echo off
-set /p version=<..\layouts\partials\version.txt
-echo %version%>metrics.%version%.txt
+setlocal enabledelayedexpansion
+set /p version=<..\layouts\partials\version.log
+echo %version%>metrics.%version%.log
 echo disableAssetsBusting=true>>config.toml
 echo disableGeneratorVersion=true>>config.toml
 echo disableRandomIds=true>>config.toml
-hugo --templateMetrics --templateMetricsHints --cleanDestinationDir --destination public.%version% >> metrics.%version%.txt
-move /Y metrics.%version%.txt public.%version%\metrics.txt 2>&1 >NUL
+hugo --templateMetrics --templateMetricsHints --cleanDestinationDir --destination public.%version% >> metrics.%version%.log
+
+set "start_dir=%CD%\public.%version%"
+set "output_file=dir.%version%.log"
+if exist "%output_file%" del "%output_file%"
+for /r "%start_dir%" %%F in (*) do (
+    set "file=%%F"
+    echo !file:%start_dir%\=! >> "%output_file%"
+)
+move /Y dir.%version%.log public.%version%\dir.log 2>&1 >NUL
+
+move /Y metrics.%version%.log public.%version%\metrics.log 2>&1 >NUL
 git restore config.toml
-For /F "UseBackQ Delims==" %%A In ("public.%version%\metrics.txt") Do Set "lastline=%%A"
+For /F "UseBackQ Delims==" %%A In ("public.%version%\metrics.log") Do Set "lastline=%%A"
 echo %lastline%
--- exampleSite/test.min.bat
+++ exampleSite/test.min.bat
@@ -1,13 +1,24 @@
 @echo off
+setlocal enabledelayedexpansion
 set hugo_major=0
 set hugo_minor=95
 set hugo_patch=0
-set /p version=<..\layouts\partials\version.txt
-echo %version%>metrics.%version%.hugo.%hugo_major%.%hugo_minor%.%hugo_patch%.txt
+set /p version=<..\layouts\partials\version.log
+echo %version%>metrics.%version%.hugo.%hugo_major%.%hugo_minor%.%hugo_patch%.log
 echo disableAssetsBusting=true>>config.toml
 echo disableGeneratorVersion=true>>config.toml
-hugo.%hugo_major%.%hugo_minor%.%hugo_patch% --templateMetrics --templateMetricsHints --cleanDestinationDir --destination public.%version%.hugo.%hugo_major%.%hugo_minor%.%hugo_patch% >> metrics.%version%.hugo.%hugo_major%.%hugo_minor%.%hugo_patch%.txt
-move /Y metrics.%version%.hugo.%hugo_major%.%hugo_minor%.%hugo_patch%.txt public.%version%.hugo.%hugo_major%.%hugo_minor%.%hugo_patch%\metrics.txt 2>&1 >NUL
+hugo.%hugo_major%.%hugo_minor%.%hugo_patch% --templateMetrics --templateMetricsHints --cleanDestinationDir --destination public.%version%.hugo.%hugo_major%.%hugo_minor%.%hugo_patch% >> metrics.%version%.hugo.%hugo_major%.%hugo_minor%.%hugo_patch%.log
+
+set "start_dir=%CD%\public.%version%.hugo.%hugo_major%.%hugo_minor%.%hugo_patch%"
+set "output_file=dir.%version%.hugo.%hugo_major%.%hugo_minor%.%hugo_patch%.log"
+if exist "%output_file%" del "%output_file%"
+for /r "%start_dir%" %%F in (*) do (
+    set "file=%%F"
+    echo !file:%start_dir%\=! >> "%output_file%"
+)
+move /Y dir.%version%.hugo.%hugo_major%.%hugo_minor%.%hugo_patch%.log public.%version%.hugo.%hugo_major%.%hugo_minor%.%hugo_patch%\dir.log 2>&1 >NUL
+
+move /Y metrics.%version%.hugo.%hugo_major%.%hugo_minor%.%hugo_patch%.log public.%version%.hugo.%hugo_major%.%hugo_minor%.%hugo_patch%\metrics.log 2>&1 >NUL
 git restore config.toml
-For /F "UseBackQ Delims==" %%A In ("public.%version%.hugo.%hugo_major%.%hugo_minor%.%hugo_patch%\metrics.txt") Do Set "lastline=%%A"
+For /F "UseBackQ Delims==" %%A In ("public.%version%.hugo.%hugo_major%.%hugo_minor%.%hugo_patch%\metrics.log") Do Set "lastline=%%A"
 echo %lastline%
