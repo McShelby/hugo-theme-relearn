ab67fcc80ea7471cc229f4ffd05b058d9633f0b8
Sören Weber <mail@soeren-weber.de>
2023-06-09 20:28:03 +0200

    test: modularize config

--- exampleSite/test.bat
+++ exampleSite/test.bat
@@ -2,10 +2,7 @@
 setlocal enabledelayedexpansion
 set /p version=<..\layouts\partials\version.txt
 echo %version%>metrics.%version%.log
-echo disableAssetsBusting=true>>config.toml
-echo disableGeneratorVersion=true>>config.toml
-echo disableRandomIds=true>>config.toml
-hugo --templateMetrics --templateMetricsHints --cleanDestinationDir --destination public.%version% >> metrics.%version%.log
+hugo --environment testing --templateMetrics --templateMetricsHints --cleanDestinationDir --destination public.%version% >> metrics.%version%.log
 
 set "start_dir=%CD%\public.%version%"
 set "output_file=dir.%version%.log"
@@ -17,6 +14,5 @@ for /r "%start_dir%" %%F in (*) do (
 move /Y dir.%version%.log public.%version%\dir.log 2>&1 >NUL
 
 move /Y metrics.%version%.log public.%version%\metrics.log 2>&1 >NUL
-git restore config.toml
 For /F "UseBackQ Delims==" %%A In ("public.%version%\metrics.log") Do Set "lastline=%%A"
 echo %lastline%
--- exampleSite/test.min.bat
+++ exampleSite/test.min.bat
@@ -5,9 +5,7 @@ set hugo_minor=95
 set hugo_patch=0
 set /p version=<..\layouts\partials\version.txt
 echo %version%>metrics.%version%.hugo.%hugo_major%.%hugo_minor%.%hugo_patch%.log
-echo disableAssetsBusting=true>>config.toml
-echo disableGeneratorVersion=true>>config.toml
-hugo.%hugo_major%.%hugo_minor%.%hugo_patch% --templateMetrics --templateMetricsHints --cleanDestinationDir --destination public.%version%.hugo.%hugo_major%.%hugo_minor%.%hugo_patch% >> metrics.%version%.hugo.%hugo_major%.%hugo_minor%.%hugo_patch%.log
+hugo.%hugo_major%.%hugo_minor%.%hugo_patch% --environment testing --templateMetrics --templateMetricsHints --cleanDestinationDir --destination public.%version%.hugo.%hugo_major%.%hugo_minor%.%hugo_patch% >> metrics.%version%.hugo.%hugo_major%.%hugo_minor%.%hugo_patch%.log
 
 set "start_dir=%CD%\public.%version%.hugo.%hugo_major%.%hugo_minor%.%hugo_patch%"
 set "output_file=dir.%version%.hugo.%hugo_major%.%hugo_minor%.%hugo_patch%.log"
@@ -19,6 +17,5 @@ for /r "%start_dir%" %%F in (*) do (
 move /Y dir.%version%.hugo.%hugo_major%.%hugo_minor%.%hugo_patch%.log public.%version%.hugo.%hugo_major%.%hugo_minor%.%hugo_patch%\dir.log 2>&1 >NUL
 
 move /Y metrics.%version%.hugo.%hugo_major%.%hugo_minor%.%hugo_patch%.log public.%version%.hugo.%hugo_major%.%hugo_minor%.%hugo_patch%\metrics.log 2>&1 >NUL
-git restore config.toml
 For /F "UseBackQ Delims==" %%A In ("public.%version%.hugo.%hugo_major%.%hugo_minor%.%hugo_patch%\metrics.log") Do Set "lastline=%%A"
 echo %lastline%
