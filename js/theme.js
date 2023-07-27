e36bc1d4f393991ffe555331692ef8ba89e162ed
McShelby <McShelby@users.noreply.github.com>
2023-07-27 19:18:53 +0000

    deploy: e44a96cb22d6bb5e5e1d6804580135cbae79818f

--- js/theme.js
+++ js/theme.js
@@ -13,6 +13,7 @@ else{
 var isPrint = document.querySelector( 'body' ).classList.contains( 'print' );
 
 var isRtl = document.querySelector( 'html' ).getAttribute( 'dir' ) == 'rtl';
+var lang = document.querySelector( 'html' ).getAttribute( 'lang' );
 var dir_padding_start = 'padding-left';
 var dir_padding_end = 'padding-right';
 var dir_key_start = 37;
@@ -341,7 +342,7 @@ function initOpenapi( update, attrs ){
         oi.classList.toggle('sc-openapi-iframe', true);
         oi.srcdoc =
             '<!doctype html>' +
-            '<html lang="en">' +
+            '<html lang="' + lang + '" dir="' + (isRtl ? 'rtl' : 'ltr') + '">' +
                 '<head>' +
                     '<link rel="stylesheet" href="' + window.themeUseOpenapi.css + '">' +
                     '<link rel="stylesheet" href="' + theme + '">' +
