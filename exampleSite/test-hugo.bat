b0c3f96b29cd29eac3bd5a3637b36709878ce930
Sören Weber <mail@soeren-weber.de>
2023-10-13 22:30:30 +0200

    theme: improve tests #685
    by
    - removing duplicate code
    - make it runnable from foreign Hugo project repos

---
+++ exampleSite/test-hugo.bat
@@ -0,0 +1,31 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set hugo_prefix=
+set hugo_version=
+if not "%~1"=="" (
+    set hugo_prefix=.hugo
+    set hugo_version=.%1
+)
+
+set "versionFile=..\layouts\partials\version.txt"
+if not exist "%versionFile%" (
+    set "versionFile=..\hugo-theme-relearn\layouts\partials\version.txt"
+)
+set /p version=<"%versionFile%"
+echo %version%>"metrics.%version%%hugo_prefix%%hugo_version%.log"
+
+hugo%hugo_version% --environment testing --templateMetrics --templateMetricsHints --cleanDestinationDir --destination "public.%version%%hugo_prefix%%hugo_version%" >> "metrics.%version%%hugo_prefix%%hugo_version%.log"
+
+set "start_dir=%CD%\public.%version%%hugo_prefix%%hugo_version%"
+set "output_file=dir.%version%%hugo_prefix%%hugo_version%.log"
+if exist "%output_file%" del "%output_file%"
+for /r "%start_dir%" %%F in (*) do (
+    set "file=%%F"
+    echo !file:%start_dir%\=! >> "%output_file%"
+)
+move /Y "dir.%version%%hugo_prefix%%hugo_version%.log" "public.%version%%hugo_prefix%%hugo_version%\dir.log" 2>&1 >NUL
+
+move /Y "metrics.%version%%hugo_prefix%%hugo_version%.log" "public.%version%%hugo_prefix%%hugo_version%\metrics.log" 2>&1 >NUL
+For /F "UseBackQ Delims==" %%A In ("public.%version%%hugo_prefix%%hugo_version%\metrics.log") Do Set "lastline=%%A"
+echo %lastline%
