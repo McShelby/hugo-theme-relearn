fcb255caf7d1386bb4bf19aa599fce6457ef70e9
Sören Weber <mail@soeren-weber.de>
2023-11-16 23:53:06 +0100

    theme: fix JS errors on hugo --minifiy #724

--- static/js/theme.js
+++ static/js/theme.js
@@ -127,21 +127,21 @@ function switchTab(tabGroup, tabId) {
 
       // Store the selection to make it persistent
       if(window.localStorage){
-          var selectionsJSON = window.localStorage.getItem(baseUriFull+"tab-selections");
+          var selectionsJSON = window.localStorage.getItem(window.relearn.baseUriFull+"tab-selections");
           if(selectionsJSON){
             var tabSelections = JSON.parse(selectionsJSON);
           }else{
             var tabSelections = {};
           }
           tabSelections[tabGroup] = tabId;
-          window.localStorage.setItem(baseUriFull+"tab-selections", JSON.stringify(tabSelections));
+          window.localStorage.setItem(window.relearn.baseUriFull+"tab-selections", JSON.stringify(tabSelections));
       }
     }
 }
 
 function restoreTabSelections() {
     if(window.localStorage){
-        var selectionsJSON = window.localStorage.getItem(baseUriFull+"tab-selections");
+        var selectionsJSON = window.localStorage.getItem(window.relearn.baseUriFull+"tab-selections");
         if(selectionsJSON){
           var tabSelections = JSON.parse(selectionsJSON);
         }else{
@@ -304,7 +304,7 @@ function initMermaid( update, attrs ) {
 
     var search;
     if( update ){
-        search = sessionStorage.getItem( baseUriFull+'search-value' );
+        search = sessionStorage.getItem( window.relearn.baseUriFull+'search-value' );
         unmark();
     }
     var is_initialized = ( update ? update_func( attrs ) : init_func( attrs ) );
@@ -352,7 +352,7 @@ function initMermaid( update, attrs ) {
         });
     }
     if( update && search && search.length ){
-        sessionStorage.setItem( baseUriFull+'search-value', search );
+        sessionStorage.setItem( window.relearn.baseUriFull+'search-value', search );
         mark();
     }
 }
@@ -1121,7 +1121,7 @@ function initExpand(){
 }
 
 function clearHistory() {
-    var visitedItem = baseUriFull + 'visited-url/'
+    var visitedItem = window.relearn.baseUriFull + 'visited-url/'
     for( var item in sessionStorage ){
         if( item.substring( 0, visitedItem.length ) === visitedItem ){
             sessionStorage.removeItem( item );
@@ -1137,7 +1137,7 @@ function clearHistory() {
 }
 
 function initHistory() {
-    var visitedItem = baseUriFull + 'visited-url/'
+    var visitedItem = window.relearn.baseUriFull + 'visited-url/'
     sessionStorage.setItem( visitedItem+document.querySelector( 'body' ).dataset.url, 1);
 
     // loop through the sessionStorage and see if something should be marked as visited
@@ -1192,7 +1192,7 @@ function scrollToPositions() {
         return;
     }
 
-    var search = sessionStorage.getItem( baseUriFull+'search-value' );
+    var search = sessionStorage.getItem( window.relearn.baseUriFull+'search-value' );
     if( search && search.length ){
         search = regexEscape( search );
         var found = elementContains( search, elc );
@@ -1229,7 +1229,7 @@ function mark() {
 		bodyInnerLinks[i].classList.add( 'highlight' );
 	}
 
-	var value = sessionStorage.getItem( baseUriFull + 'search-value' );
+	var value = sessionStorage.getItem( window.relearn.baseUriFull + 'search-value' );
     var highlightableElements = document.querySelectorAll( '.highlightable' );
     highlight( highlightableElements, value, { element: 'mark' } );
 
@@ -1317,7 +1317,7 @@ function highlightNode( node, re, nodeName, className ){
 };
 
 function unmark() {
-	sessionStorage.removeItem( baseUriFull + 'search-value' );
+	sessionStorage.removeItem( window.relearn.baseUriFull + 'search-value' );
 	var markedElements = document.querySelectorAll( 'mark' );
 	for( var i = 0; i < markedElements.length; i++ ){
 		var parent = markedElements[i].parentNode;
@@ -1383,7 +1383,7 @@ function elementContains( txt, e ){
 function searchInputHandler( value ){
     unmark();
     if( value.length ){
-        sessionStorage.setItem( baseUriFull+'search-value', value );
+        sessionStorage.setItem( window.relearn.baseUriFull+'search-value', value );
         mark();
     }
 }
@@ -1395,7 +1395,7 @@ function initSearch() {
         e.addEventListener( 'keydown', function( event ){
             if( event.key == 'Escape' ){
                 var input = event.target;
-                var search = sessionStorage.getItem( baseUriFull+'search-value' );
+                var search = sessionStorage.getItem( window.relearn.baseUriFull+'search-value' );
                 if( !search || !search.length ){
                     input.blur();
                 }
@@ -1435,13 +1435,13 @@ function initSearch() {
     var urlParams = new URLSearchParams( window.location.search );
     var value = urlParams.get( 'search-by' );
     if( value ){
-        sessionStorage.setItem( baseUriFull+'search-value', value );
+        sessionStorage.setItem( window.relearn.baseUriFull+'search-value', value );
     }
     mark();
 
     // set initial search value for inputs on page load
-    if( sessionStorage.getItem( baseUriFull+'search-value' ) ){
-        var search = sessionStorage.getItem( baseUriFull+'search-value' );
+    if( sessionStorage.getItem( window.relearn.baseUriFull+'search-value' ) ){
+        var search = sessionStorage.getItem( window.relearn.baseUriFull+'search-value' );
         inputs.forEach( function( e ){
             e.value = search;
             var event = document.createEvent( 'Event' );
