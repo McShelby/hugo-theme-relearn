24c7258eb44f15073c095e6581dc5ecc7d0e0d2c
Sören Weber <mail@soeren-weber.de>
2024-04-07 21:41:06 +0200

    theme: dry out title generation #825

--- layouts/_default/index.search.js
+++ layouts/_default/index.search.js
@@ -2,17 +2,9 @@
 {{- $pages := slice }}
 {{- range .Site.Pages }}
   {{- if and .Title .RelPermalink (or (ne (.Scratch.Get "relearnIsHiddenStem") true) (ne .Site.Params.disableSearchHiddenPages true) ) }}
-    {{- $title := .Title }}
-    {{- if eq .Kind "taxonomy" }}
-      {{- $title = default (default .Data.Plural (i18n .Data.Plural)) .Params.Title }}
-    {{- else if eq .Kind "term" }}
-      {{- $taxonomy_page := .Site.GetPage .Data.Plural }}
-      {{- $title = default (default .Data.Singular (i18n .Data.Singular)) $taxonomy_page.Params.SingularTitle }}
-      {{- $title = printf "%s %s %s" $title (default "::" .Site.Params.titleSeparator) (default (humanize .Data.Term | strings.Title) .Title) }}
-    {{- end }}
     {{- $pages = $pages | append (dict
       "uri" (partial "relLangPrettyUglyURL.hugo" (dict "to" .))
-      "title" $title
+      "title" (partial "pageHelper/title.hugo" (dict "page" .))
       "tags" .Params.tags
       "breadcrumb" (trim ((partial "breadcrumbs.html" (dict "page" . "dirOnly" true)) | plainify | htmlUnescape) "\n\r\t ")
       "description" .Description
