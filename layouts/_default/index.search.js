64b4ad751ef8922f6fb57c7c4c785bdf345c91f9
Sören Weber <mail@soeren-weber.de>
2024-04-27 14:38:19 +0200

    theme: sync Hugo templates and unification #841 #845
    - description
    - title
    - tags

--- layouts/_default/index.search.js
+++ layouts/_default/index.search.js
@@ -1,14 +1,20 @@
 {{- partialCached "page-meta.hugo" . .RelPermalink }}
+{{- $format := partial "get-format.hugo" . }}
+{{- $outputFormat := partial "output-format.hugo" (dict "page" . "format" $format) }}
 {{- $pages := slice }}
 {{- range .Site.Pages }}
   {{- if and .Title .RelPermalink (or (ne (.Scratch.Get "relearnIsHiddenStem") true) (ne .Site.Params.disableSearchHiddenPages true) ) }}
+    {{- $tags := slice }}
+    {{- range .GetTerms "tags" }}
+      {{- $tags = $tags | append (partial "pageHelper/title.hugo" (dict "page" .Page "linkTitle" true "format" $format "outputFormat" $outputFormat) | plainify) }}
+    {{- end }}
     {{- $pages = $pages | append (dict
       "uri" (partial "relLangPrettyUglyURL.hugo" (dict "to" .))
-      "title" (partial "pageHelper/title.hugo" (dict "page" .))
-      "tags" .Params.tags
-      "breadcrumb" (trim ((partial "breadcrumbs.html" (dict "page" . "dirOnly" true)) | plainify | htmlUnescape) "\n\r\t ")
-      "description" (or .Description .Summary)
-      "content" (.Plain | htmlUnescape)
+      "title" (partial "pageHelper/title.hugo" (dict "page" . "format" $format "outputFormat" $outputFormat) | plainify)
+      "tags" $tags
+      "breadcrumb" (partial "breadcrumbs.html" (dict "page" . "dirOnly" true) | plainify | htmlUnescape | chomp)
+      "description" (or .Description .Summary | plainify | htmlUnescape | chomp)
+      "content" (.Plain | htmlUnescape | chomp)
     ) }}
   {{- end }}
 {{- end -}}
