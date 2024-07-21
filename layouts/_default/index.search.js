926bea47272aaeca556b639ad4834f9b77df3d16
Sören Weber <mail@soeren-weber.de>
2024-07-22 00:15:38 +0200

    title: avoid to much dependency on outputFormat #685

--- layouts/_default/index.search.js
+++ layouts/_default/index.search.js
@@ -1,16 +1,14 @@
 {{- partialCached "page-meta.hugo" . .RelPermalink }}
-{{- $format := partial "get-format.hugo" . }}
-{{- $outputFormat := partial "output-format.hugo" (dict "page" . "format" $format) }}
 {{- $pages := slice }}
 {{- range .Site.Pages }}
   {{- if and .Title .RelPermalink (or (ne (.Scratch.Get "relearnIsHiddenStem") true) (ne .Site.Params.disableSearchHiddenPages true) ) }}
     {{- $tags := slice }}
     {{- range .GetTerms "tags" }}
-      {{- $tags = $tags | append (partial "pageHelper/title.hugo" (dict "page" .Page "linkTitle" true "format" $format "outputFormat" $outputFormat) | plainify) }}
+      {{- $tags = $tags | append (partial "pageHelper/title.hugo" (dict "page" .Page "linkTitle" true) | plainify) }}
     {{- end }}
     {{- $pages = $pages | append (dict
       "uri" (partial "relLangPrettyUglyURL.hugo" (dict "to" .))
-      "title" (partial "pageHelper/title.hugo" (dict "page" . "format" $format "outputFormat" $outputFormat) | plainify)
+      "title" (partial "pageHelper/title.hugo" (dict "page" .) | plainify)
       "tags" $tags
       "breadcrumb" (partial "breadcrumbs.html" (dict "page" . "dirOnly" true) | plainify | htmlUnescape | chomp)
       "description" (or .Description .Summary | plainify | htmlUnescape | chomp)
