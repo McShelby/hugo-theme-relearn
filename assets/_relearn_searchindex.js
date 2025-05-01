c442e9fa5f6dfbdd43bd3c5a4915265c6c60c8a2
Sören Weber <mail@soeren-weber.de>
2025-05-01 11:58:22 +0200

    theme: remove recursive partial cached calls #1086
    this is a pitty and necessary since Hugo 0.146.7
    https://discourse.gohugo.io/t/circular-call-stack-detected-for-recursive-partialcached-since-0-146-7/54559/5
    maybe in a later release they may fix it but for now I am tired of discussing

--- assets/_relearn_searchindex.js
+++ assets/_relearn_searchindex.js
@@ -1,7 +1,9 @@
 {{- $pages := slice }}
 {{- range .Site.Pages }}
   {{- if partial "_relearn/pageIsSpecial.gotmpl" . }}
-  {{- else if and .Title .RelPermalink (or (ne .Site.Params.disableSearchHiddenPages true) (not (partialCached "_relearn/pageIsHiddenSelfOrAncestor.gotmpl" (dict "page" . "to" .Site.Home) .Path .Site.Home.Path) ) ) }}
+  {{- /* This wants to be partialCached but can't since Hugo 0.146.7 }}
+  {{- else if and .Title .RelPermalink (or (ne .Site.Params.disableSearchHiddenPages true) (not (partialCached "_relearn/pageIsHiddenSelfOrAncestor.gotmpl" (dict "page" . "to" .Site.Home) .Path .Site.Home.Path) ) ) */}}
+  {{- else if and .Title .RelPermalink (or (ne .Site.Params.disableSearchHiddenPages true) (not (partial "_relearn/pageIsHiddenSelfOrAncestor.gotmpl" (dict "page" . "to" .Site.Home) .Path .Site.Home.Path) ) ) }}
     {{- $tags := slice }}
     {{- range .GetTerms "tags" }}
       {{- $tags = $tags | append (partial "title.gotmpl" (dict "page" .Page "linkTitle" true) | plainify) }}
