0ab4f2e2c3d70fea6c0f6b4ff03389d358302c46
Sören Weber <mail@soeren-weber.de>
2025-12-05 16:54:54 +0100

    theme: remove fix for some Hugo versions #1175

--- assets/_relearn_searchindex.js
+++ assets/_relearn_searchindex.js
@@ -1,9 +1,7 @@
 {{- $pages := slice }}
 {{- range .Site.Pages }}
   {{- if partial "_relearn/pageIsSpecial.gotmpl" . }}
-  {{- /* This wants to be partialCached but can't since Hugo 0.146.7 }}
-  {{- else if and .Title .RelPermalink (or (ne .Site.Params.disableSearchHiddenPages true) (not (partialCached "_relearn/pageIsHiddenSelfOrAncestor.gotmpl" (dict "page" . "to" .Site.Home) .Path .Site.Home.Path) ) ) */}}
-  {{- else if and .Title .RelPermalink (or (ne .Site.Params.disableSearchHiddenPages true) (not (partial "_relearn/pageIsHiddenSelfOrAncestor.gotmpl" (dict "page" . "to" .Site.Home) .Path .Site.Home.Path) ) ) }}
+  {{- else if and .Title .RelPermalink (or (ne .Site.Params.disableSearchHiddenPages true) (not (partialCached "_relearn/pageIsHiddenSelfOrAncestor.gotmpl" (dict "page" . "to" .Site.Home) .Path .Site.Home.Path) ) ) }}
     {{- $tags := slice }}
     {{- range .GetTerms "tags" }}
       {{- $tags = $tags | append (partial "title.gotmpl" (dict "page" .Page "linkTitle" true) | plainify) }}
